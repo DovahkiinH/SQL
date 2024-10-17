6c70a9a49cbd12a90cdf6bc63586129e376e5239
DovahkiinH <loick.caplier@gmail.com>
2024-10-17 16:36:41 +0200

    requetes

--- Papyrus.sql
+++ Papyrus.sql
@@ -11,4 +11,39 @@ FROM fournis
 JOIN entcom ON fournis.numfou = entcom.numfou; 
 
 --3
+SELECT COUNT(DISTINCT e.numcom) AS nombre_commandes, COUNT(DISTINCT e.numfou) AS nombre_fournisseurs 
+FROM entcom e;
 
+--4
+
+--5
+
+--6
+
+--7
+
+--8
+
+--9
+
+--10
+
+--11
+
+--12
+
+--13
+
+--14
+
+--15
+
+--16
+
+--17
+
+--18
+
+--19
+
+--20
